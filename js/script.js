ea7a8e079e2db2fa415d51cabe475c2ccf98f0af
aproskur <ao.panova@gmail.com>
2021-06-30 22:46:13 +0100

    illustration js + more works in portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-window.addEventListener('scroll', () => {
-  console.log("Scrolled!");
+let cards = document.querySelectorAll('.card');
+let modal_image = document.querySelector('.full-image');
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    let bigSrc = card.getAttribute('data-original');
+    modal_image.src = `${bigSrc}`;
+  });
 });
