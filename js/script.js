490066d7a59abd62e517111dc6fe5c764ce1e11e
aproskur <ao.panova@gmail.com>
2021-07-05 11:05:57 +0100

    test js2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let cards = document.querySelectorAll('.card');
 let modal_image = document.querySelector('.full-image');
+let mdi = document.querySelector('.dynamic_modal_image')
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
-    $('content').empty();
+    mdi.setAttribute("src", "");
+    console.log(mdi);
     let bigSrc = card.getAttribute('data-original');
     modal_image.src = `${bigSrc}`;
   });
