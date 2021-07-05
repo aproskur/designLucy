9a668c60ed77c9da032c43eda4ec62131efa602f
aproskur <ao.panova@gmail.com>
2021-07-05 11:18:03 +0100

    new pics

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ let modal_image = document.querySelector('.full-image');
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
-    modal_image.src = `""`;
-    console.log(mdi);
+    modal_image.src = "";
     let bigSrc = card.getAttribute('data-original');
     modal_image.src = `${bigSrc}`;
   });
