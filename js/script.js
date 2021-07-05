d22153f541fed004df8f282d38e03cb37abb093d
aproskur <ao.panova@gmail.com>
2021-07-05 10:49:45 +0100

    test js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let modal_image = document.querySelector('.full-image');
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
+    $('content').empty();
     let bigSrc = card.getAttribute('data-original');
     modal_image.src = `${bigSrc}`;
   });
