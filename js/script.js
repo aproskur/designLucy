0dbd055b60779588c75479022d2bb9593e82d694
aproskur <ao.panova@gmail.com>
2021-07-05 11:12:50 +0100

    addition

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 let cards = document.querySelectorAll('.card');
 let modal_image = document.querySelector('.full-image');
-let mdi = document.querySelector('.dynamic_modal_image')
+
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
-    mdi.setAttribute("src", "");
+    modal_image.src = `""`;
     console.log(mdi);
     let bigSrc = card.getAttribute('data-original');
     modal_image.src = `${bigSrc}`;
